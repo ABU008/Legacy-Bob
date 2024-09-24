acce357edb50518494d8d9016ebc9c7580324feb
ABU008 <abubakrmohsin2010@gmail.com>
2024-09-24 11:14:29 +0100

    fixed the falling to flying transition, camera tilts down slightly as your falling and fixed chunks disappearing behind you when falling.

--- common/src/main/java/legacybob/oebwf2ij/mixin/GameRendererMixin.java
+++ common/src/main/java/legacybob/oebwf2ij/mixin/GameRendererMixin.java
@@ -4,6 +4,7 @@ import com.mojang.math.Axis;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,14 +13,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import legacybob.oebwf2ij.Bobbing.LegacyBobbing;
 @Mixin(GameRenderer.class)
-public abstract class GameRendererMixin {
+public abstract class GameRendererMixin implements LegacyBobbing {
 
     @Final
     @Shadow
     Minecraft minecraft;
-
     @Inject(method = "bobView", at = @At("RETURN"))
-    private void bobView(PoseStack poseStack, float f, CallbackInfo ci){
-        if (this.minecraft.getCameraEntity() instanceof LegacyBobbing p && !minecraft.player.getAbilities().flying) poseStack.mulPose(Axis.XP.rotationDegrees(p.getAngle(f)));
+    private void bobView(PoseStack poseStack, float partialTick, CallbackInfo ci) {
+        if (this.minecraft.getCameraEntity() instanceof LegacyBobbing p) {
+            LegacyBobbing legacyBobbing = (LegacyBobbing) p;
+            float angle = Mth.lerp(partialTick, legacyBobbing.$prevYBob(), legacyBobbing.$yBob());
+
+            poseStack.mulPose(Axis.XP.rotationDegrees(angle));
+        }
     }
 }
