e7dd7647dd797cea7f06c9a4390c59456fef2e09
ABU008 <abubakrmohsin2010@gmail.com>
2024-09-24 19:10:10 +0100

    initialised viewvector properly for reliability purposes

--- common/src/main/java/legacybob/oebwf2ij/mixin/FrustumMixin.java
+++ common/src/main/java/legacybob/oebwf2ij/mixin/FrustumMixin.java
@@ -3,6 +3,7 @@ package legacybob.oebwf2ij.mixin;
 import net.minecraft.client.renderer.culling.Frustum;
 import org.joml.Matrix4f;
 import org.joml.FrustumIntersection;
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.*;
 
 @Mixin(Frustum.class)
@@ -15,6 +16,8 @@ public class FrustumMixin {
     @Final
     @Shadow
     private Matrix4f matrix;
+    @Shadow
+    private Vector4f viewVector;
     /**
      * @author ABU008
      * @reason To fix culling issues when falling
@@ -27,6 +30,9 @@ public class FrustumMixin {
         adjustedProjectionMatrix.mul(viewMatrix, this.matrix);
 
         this.intersection.set(this.matrix);
+
+        this.viewVector = new Vector4f(0.0F, 0.0F, 1.0F, 0.0F);
+        this.viewVector = this.matrix.transformTranspose(this.viewVector);
     }
 
     @Unique
