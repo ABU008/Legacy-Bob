a3613f3d818ab62b69b42537e0c2fda995f1a3b7
ABU008 <abubakrmohsin2010@gmail.com>
2025-05-04 20:48:50 +0800

    revert FrustumMixin.java

--- common/src/main/java/legacybob/oebwf2ij/mixin/FrustumMixin.java
+++ common/src/main/java/legacybob/oebwf2ij/mixin/FrustumMixin.java
@@ -1,37 +1,48 @@
 package legacybob.oebwf2ij.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.culling.Frustum;
 import org.joml.Matrix4f;
+import org.joml.FrustumIntersection;
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.At;
-
-import legacybob.oebwf2ij.Bobbing.LegacyBobbing;
 
 @Mixin(Frustum.class)
 public class FrustumMixin {
 
     @Final
     @Shadow
+    private FrustumIntersection intersection;
+
+    @Final
+    @Shadow
     private Matrix4f matrix;
 
-    @Unique
-    private final Camera legacybob$mainCamera = new Camera();
+    @Shadow private Vector4f viewVector;
 
-    @ModifyExpressionValue(method = "calculateFrustum", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;mul(Lorg/joml/Matrix4fc;Lorg/joml/Matrix4f;)Lorg/joml/Matrix4f;", remap = false))
-    private Matrix4f calculateFrustum(Matrix4f original, Matrix4f right, Matrix4f matrix4f2) {
-        float angle = LegacyBobbing.getAngle(Minecraft.getInstance(), this.legacybob$mainCamera.getPartialTickTime());
-        return angle != 0 ? legacybob$adjustByBobbing(matrix4f2, angle).mul(right, matrix) : original;
+    /**
+     * @author ABU008
+     * @reason To fix culling issues when falling
+     */
+    @Overwrite
+    private void calculateFrustum(Matrix4f viewMatrix, Matrix4f projectionMatrix) {
+
+        Matrix4f adjustedProjectionMatrix = legacybob$adjustProjectionFOV(projectionMatrix);
+
+        adjustedProjectionMatrix.mul(viewMatrix, this.matrix);
+
+        this.intersection.set(this.matrix);
+
+        this.viewVector = new Vector4f(0.0F, 0.0F, 1.0F, 0.0F);
+        this.viewVector = matrix.transformTranspose(viewVector);
     }
 
     @Unique
-    private Matrix4f legacybob$adjustByBobbing(Matrix4f projectionMatrix, float yBobAngle) {
-        float mul = Math.max(0.65f, 1f - yBobAngle / 32);
+    private Matrix4f legacybob$adjustProjectionFOV(Matrix4f projectionMatrix) {
         Matrix4f adjustedMatrix = new Matrix4f(projectionMatrix);
-        adjustedMatrix.m00(adjustedMatrix.m00() * mul);
-        adjustedMatrix.m11(adjustedMatrix.m11() * mul);
+
+        adjustedMatrix.m00(adjustedMatrix.m00() * 0.65f);
+        adjustedMatrix.m11(adjustedMatrix.m11() * 0.65f);
+
         return adjustedMatrix;
     }
 }
